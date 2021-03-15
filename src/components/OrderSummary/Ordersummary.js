47ac04e598b383bdb481d2aa9b815691757910f4
AndyMMunro <agovitadventures@gmail.com>
2021-03-15 12:48:41 -0700

    component did update added to the modal for preformance

--- src/components/OrderSummary/Ordersummary.js
+++ src/components/OrderSummary/Ordersummary.js
@@ -1,34 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Button from '../UI/Button/Button';
 
-const orderSummary = (props) => {
+class OrderSummary extends Component {
 
-  const ingredientSummary = Object.keys(props.ingredients)
+  // componentDidUpdate(){
+  //   console.log('[OrderSummary] Did up date');
+  // }
+
+  render () {
+
+    const ingredientSummary = Object.keys(this.props.ingredients)
         .map(ingKey => {
           // rapping in the span capitalizes the key name 
           return(
             <li key={ingKey}>
               <span style={{textTransform: 'capitalize' }}> {ingKey} </span> 
-              {props.ingredients[ingKey]} 
+              {this.props.ingredients[ingKey]} 
             </li>
           ) 
         });
 
-  return(
+    return(    
     <Aux>
       <h3>Your Order </h3>
       <p>Scrumptious Burger with:</p>
       <ul>
         {ingredientSummary}
       </ul>
-      <p><strong>TOTAL PRICE: ${props.totalPrice.toFixed(2)} </strong></p>
+      <p><strong>TOTAL PRICE: ${this.props.totalPrice.toFixed(2)} </strong></p>
       <p>Continue to check out?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelHandler} >CANCEL ORDER</Button>
-      <Button btnType="Success" clicked={props.purchaseSuccessHandler}  >CONTINUE TO CHECKOUT</Button>
+      <Button btnType="Danger" clicked={this.props.purchaseCancelHandler} >CANCEL ORDER</Button>
+      <Button btnType="Success" clicked={this.props.purchaseSuccessHandler}  >CONTINUE TO CHECKOUT</Button>
     </Aux>
+    );
+  }
+} 
 
-  )
-};
-
-export default orderSummary;
+export default OrderSummary;
