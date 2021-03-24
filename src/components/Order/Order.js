bef80bf3114ca036749155687566a2dd843a904d
AndyMMunro <agovitadventures@gmail.com>
2021-03-23 22:59:19 -0700

    order page renders ingredients as well as price from the data base

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,13 +1,46 @@
 import React from "react";
 import classes from "./Order.module.css";
 
-const order = (props) => (
-    <div className={classes.Order}>
-        <p>Ingredients:</p>
-        <p>
-            Price: <strong>USD $ {props.price} </strong>
-        </p>
-    </div>
-);
+const order = (props) => {
+    // here was are taking the JS object and converting it into an array so
+    // that it can be rendered to the app
+    const ingredients = [];
+
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            name: ingredientName,
+            amount: props.ingredients[ingredientName],
+        });
+    }
+
+    const ingredientOutput = ingredients.map((ing) => {
+        return (
+            <span
+                style={{
+                    textTransform: "capitalize",
+                    display: "inline-block",
+                    margin: "0 8px",
+                    border: "2px solid #ccc",
+                    padding: "5px",
+                }}
+                key={ing.name}
+            >
+                {ing.name} ({ing.amount})
+            </span>
+        );
+    });
+
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredientOutput} </p>
+            <p>
+                Price:
+                <strong>
+                    USD $ {Number.parseFloat(props.price).toFixed(2)}
+                </strong>
+            </p>
+        </div>
+    );
+};
 
 export default order;
